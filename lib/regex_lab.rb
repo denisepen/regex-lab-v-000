7ef68b808a7b7252f1cfe40646f4f1c7b8095f71
Denise Pendleton <dpendl1@hotmail.com>
2018-02-23 15:52:40 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,11 @@
 def starts_with_a_vowel?(word)
-
+  word_array = []
+  word_array << word.scan(/[aeiou]/)
+  if word_array = []
+    false
+  else true
+  end
+  
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
